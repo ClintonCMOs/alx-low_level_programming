73069ab5f3fab82c7ca7aeecbbb36b04bb060844
ClintonCMOs <orengecmo@gmail.com>
2023-08-11 15:40:16 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number + '0');
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
