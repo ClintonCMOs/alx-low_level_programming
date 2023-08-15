71d04ce64443134c70e98c78139e1b487151043b
ClintonCMOs <orengecmo@gmail.com>
2023-08-15 16:22:12 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,10 +10,11 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		return(1);
+		return (1);
 	}
 	else if (n < 0)
-	{ _putchar(45);
+	{
+	       	_putchar(45);
 		return (-1);
 	}
 	else
