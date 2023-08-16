b8f43d2a3196ebc0e8efe9ce5f6826092c207db4
ClintonCMOs <orengecmo@gmail.com>
2023-08-15 17:22:41 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - printing last digit
+ * @v: number to be treated
+ *
+ * Return: value of number
+ */
+int print_last_digit(int v)
+{
+	int l;
+
+	l = v % 10;
+	if (l < 0)
+	{
+		l = l * -1;
+	}
+	_putchar(l +'0');
+	return (l);
+}
