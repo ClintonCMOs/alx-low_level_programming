b3b75d394c7f8d58d2a0a20e6570e497a888303c
ClintonCMOs <orengecmo@gmail.com>
2023-08-15 18:01:43 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add integers
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: Sum of tow numbers
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
