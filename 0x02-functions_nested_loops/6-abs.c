47cfc54a915da87676cc6f565b168d85ef496fb3
ClintonCMOs <orengecmo@gmail.com>
2023-08-15 17:04:07 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @n: Number to be checked
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
