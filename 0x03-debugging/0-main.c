769bd3548210415158e42c8dfc51b4efe39c8dda
ClintonCMOs <orengecmo@gmail.com>
2023-08-17 11:56:06 -0700

    0-main debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests and prints if an ingeer is positive or negative
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
