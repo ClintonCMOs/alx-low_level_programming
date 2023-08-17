067c956d053ca4018ebc2f3cae9ad98c5bab0163
ClintonCMOs <orengecmo@gmail.com>
2023-08-17 05:56:35 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - mulriplies two integers a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
