234978b27b5b577c9472b43a27594959cb215d20
ClintonCMOs <orengecmo@gmail.com>
2023-08-21 13:53:25 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strcpy - copies string pointed
+ * @dest: destination to copy strings
+ * @src: src
+ *
+ * Return: A character
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; a < _strlen(src); a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
