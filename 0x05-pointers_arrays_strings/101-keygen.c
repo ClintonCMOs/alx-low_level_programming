18db85ff793608db5f6ae67fefc5a07f4aec28ab
ClintonCMOs <orengecmo@gmail.com>
2023-08-21 14:56:19 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,10 +13,9 @@ int main(void)
 	int i = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
-	
 	while (sum < 2772)
 	{
-		password[i=] = 33 + rand() % 94;
+		password[i] = 33 + rand() % 94;
 		sum += password[i++];
 	}
 
