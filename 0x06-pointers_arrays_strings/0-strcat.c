0149d81957480690d8646f9e79312471daae1146
ClintonCMOs <orengecmo@gmail.com>
2023-08-24 04:26:18 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,13 @@ char *_strcat(char *dest, char *src)
 	int a;
 	int b;
 
-	for (b = 0; dest[b] != '\0'; b++)
+	for (a = 0; dest[a] != '\0'; a++)
 	{
 	}
-	for (a = 0; src[a] != '\0'; a++)
+	for (b = 0; src[b] != '\0'; b++)
 	{
-		dest[b + a] = src[a];
+		dest[a + b] = src[b];
 	}
-	dest[b + a] ='\0';
+	dest[a + b] = '\0';
 	return (dest);
 }
