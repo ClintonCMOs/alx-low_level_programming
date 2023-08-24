081d04a44eac131c6a3f732a5f67270095879af9
ClintonCMOs <orengecmo@gmail.com>
2023-08-24 09:27:31 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - converts string to uppercase
+ * @s: string to be converted to uppercase
+ * Return: str
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
