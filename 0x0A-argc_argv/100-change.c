4c3989e45c0797f53a2a49bab04cdff0ccb23082
ClintonCMOs <orengecmo@gmail.com>
2023-09-01 15:07:50 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main program
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int coins;
+	int cents;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	coins = 0;
+	coins += cents / 25;
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
+	coins += cents;
+	printf("%d\n", coins);
+	return (0);
+}
