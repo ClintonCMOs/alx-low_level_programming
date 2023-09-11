555687eae3c6bf54bb8d5b49385970cd2b8ec6b8
ClintonCMOs <orengecmo@gmail.com>
2023-09-11 16:42:55 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,13 +14,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog_ptr;
 
-	new_dog_ptr = malloc(sizeof(dog_t));
+	new_dog_ptr = (dog_t *)malloc(sizeof(dog_t));
 	if (new_dog_ptr == NULL)
 	{
 		return (NULL);
 	}
-	new_dog_ptr->name = strdup(name);
-	new_dog_ptr->owner = strdup(owner);
+	new_dog_ptr->name = (char *)malloc(strlen(name) + 1);
+	new_dog_ptr->owner = (char *)malloc(strlen(owner) + 1);
 	if (new_dog_ptr->name == NULL || new_dog_ptr->owner == NULL)
 	{
 		free(new_dog_ptr->name);
@@ -28,6 +28,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog_ptr);
 		return (NULL);
 	}
+	strcpy(new_dog_ptr->name, name);
+	strcpy(new_dog_ptr->owner, owner);
 	new_dog_ptr->age = age;
 	return (new_dog_ptr);
 }
