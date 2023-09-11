b873cc3b54a6f70d0985126c64a4c082b1c2f188
ClintonCMOs <orengecmo@gmail.com>
2023-09-11 15:55:26 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A new type with 3 elements
+ * @name: my dog's name
+ * @age: my dog's age in years
+ * @owner: Dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
