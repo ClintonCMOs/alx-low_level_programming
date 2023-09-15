dfded215b8d54b416e222bdcb33b60208b35b634
ClintonCMOs <orengecmo@gmail.com>
2023-09-14 19:46:52 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,54 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
-
+#include <stdlib.h>
 /**
- * print_all - Prints anything based on the format string
- * @format: The format string containing type specifiers
- * @...: The variable number of arguments to print.
- */
-
+  *print_all - The function that prints anything
+  *
+  *@format: Arguments passed to the function
+  *
+  *@...: variable number of arguments
+  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i = 0;
-	char *separator = "";
 	char *str;
+	unsigned int a = 0;
+	va_list args;
 
 	va_start(args, format);
-
-	while (format && format[i])
+	while (format == NULL)
 	{
-		if (format[i] == 'c')
-		{
-			printf("%s%c", separator, va_arg(args, int));
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%s%d", separator, va_arg(args, int));
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%s%f", separator, va_arg(args, double));
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-
-			if (str == NULL)
-			{
-				str = "(nil)";
-			}
-			printf("%s%s", separator, str);
-		}
-		separator = ", ";
-		i++;
+		printf("\n");
+		return;
 	}
-	printf("\n");
-	va_end(args);
+	while (format[a] != '\0')
+	{
+		switch (format[a])
+		{
+			case 'c':
+				printf("%c", (char)va_arg(args, int));
+				break;
+			case 'i':
+			  printf("%d", va_arg(args, int));
+					break;
+			case 'f':
+				printf("%f", (float)va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char*);
+				if (str != NULL)
+				{
+					printf("%s", str);
+					break;
+				}
+				printf("(nil)");
+				break;
+
+		}
+		if ((format[a] == 'c' || format[a] == 'i' || format[a] == 'f' ||
+		     format[a] == 's') && format[a + 1] != '\0')
+			printf(", ");
+		a++;
+	}
+	printf("\n"), va_end(args);
 }
