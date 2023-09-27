f16e0c2678ed1395a7e32b15ceab049f269f07bf
ClintonCMOs <orengecmo@gmail.com>
2023-09-27 03:00:08 +0300

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,34 +1,89 @@
 #include "lists.h"
+/**
+ * loop_count - Counts the number of unique node_count
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: 0 (not looped),the number of unique node_count in the list
+ */
+
+size_t loop_count(listint_t *head)
+{
+	listint_t *start, *end;
+
+	size_t node_count;
+
+	node_count = 1;
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	start = head->next;
+	end = (head->next)->next;
+
+	while (end)
+	{
+		if (start == end)
+		{
+			start = head;
+			while (start != end)
+			{
+				node_count++;
+				start = start->next;
+				end = end->next;
+			}
+
+			start = start->next;
+			while (start != end)
+			{
+				node_count++;
+				start = start->next;
+			}
+
+			return (node_count);
+		}
+
+		start = start->next;
+		end = (end->next)->next;
+	}
+
+	return (0);
+}
 
 /**
- * free_listint_safe - Frees a listint_t linked list safely.
+ * free_listint_safe - safely frees a list
  * @h: A pointer to a pointer to the head node.
- * Return: the size of the list freed
+ * Return: size of freed list
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *tmp_ptr, *original;
-	size_t a;
-	int remain;
+	listint_t *temp_ptr;
 
-	original = *h;
-	for (a = 0; original; a++)
+	size_t node_count, a;
+
+	node_count = loop_count(*h);
+
+	if (node_count == 0)
 	{
-		remain = original - original->next;
-		if (remain > 0)
+		for (; h != NULL && *h != NULL; node_count++)
 		{
-			tmp_ptr = original->next;
-			free(original);
-			original = tmp_ptr;
-		}
-		else
-		{
-			free(original);
-			*h = NULL;
-			break;
+			temp_ptr = (*h)->next;
+			free(*h);
+			*h = temp_ptr;
 		}
 	}
-	*h = NULL;
-	return (a);
-}
 
+	else
+	{
+		for (a = 0; a < node_count; a++)
+		{
+			temp_ptr = (*h)->next;
+			free(*h);
+			*h = temp_ptr;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (node_count);
+}
