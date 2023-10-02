c6ed2bd96c7e98c86e1bb7615d58f0606fc57299
ClintonCMOs <orengecmo@gmail.com>
2023-10-02 19:16:32 +0300

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,56 +1,80 @@
 #include "main.h"
+#define BUFFER_SIZE 1024
+
+void print_error_and_exit(int code, const char *msg);
+int open_file(const char *filename, int mode, mode_t permissions);
+void copy_file(int src_fd, int dest_fd);
+
+/**
+ * print_error_and_exit - Print an error message and exit with the given code.
+ * @code: The error code to exit with.
+ * @msg: The error message to print.
+ */
+void print_error_and_exit(int code, const char *msg)
+{
+	dprintf(STDERR_FILENO, "Error: %s\n", msg);
+	exit(code);
+}
+
+/**
+ * open_file - Open a file with the specified mode and permissions.
+ * @filename: The name of the file to open.
+ * @mode: The mode to open the file with.
+ * @permissions: The permissions to set for the file.
+ *
+ * Return: The file descriptor of the opened file, or -1 on error.
+ */
+int open_file(const char *filename, int mode, mode_t permissions)
+{
+	int fd = open(filename, mode, permissions);
+
+	if (fd == -1)
+		print_error_and_exit(99, "Can't open file");
+
+	return (fd);
+}
+
+/**
+ * copy_file - Copy the contents of one file to another.
+ * @src_fd: The source file descriptor.
+ * @dest_fd: The destination file descriptor.
+ */
+void copy_file(int src_fd, int dest_fd)
+{
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(dest_fd, buffer, bytes_read);
+		if (bytes_written == -1)
+			print_error_and_exit(99, "Can't write to destination file");
+	}
+	if (bytes_read == -1)
+		print_error_and_exit(98, "Can't read from source file");
+}
 /**
  * main - Copies the content of a file to another file.
- * @ac: The number of command-line arguments.
- * @av: An array of command-line argument strings.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
  *
  * Return: 0 on success, or an error code on failure.
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	int fdFrum, fdToo, wrote, readed;
-	char buff[1024];
+	int src_fd, dest_fd;
 
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fdFrum = open(av[1], O_RDONLY);
-	if (fdFrum == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	fdToo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fdToo == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	while ((readed = read(fdFrum, buff, 1024)) > 0)
-	{
-		wrote = write(fdToo, buff, readed);
-		if (wrote == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
-	}
-	if (readed == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	if (close(fdFrum) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdFrum);
-		exit(100);
-	}
-	if (close(fdToo) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdToo);
-		exit(100);
-	}
+	if (argc != 3)
+		print_error_and_exit(97, "Usage: cp file_from file_to");
+
+	src_fd = open_file(argv[1], O_RDONLY, 0664);
+	dest_fd = open_file(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	copy_file(src_fd, dest_fd);
+
+	if (close(src_fd) == -1 || close(dest_fd) == -1)
+		print_error_and_exit(100, "Can't close file descriptors");
+
 	return (0);
 }
+
